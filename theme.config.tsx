de217e634741795085a94a7db44b074bcb5e1d8c
STZ0214 <steven.zhang.0214@gmail.com>
2025-02-22 16:56:56 -0500

    update

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: (
+    <span style={{ fontWeight: 600, fontSize: '1.2rem' }}>
+      🎱 Master Q Snooker, Billiards & Lounge
+    </span>
+  ),
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
@@ -11,8 +15,23 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: (
+      <div className="footer-content">
+        <div>© {new Date().getFullYear()} Master Q Billiards</div>
+        <div>20 Apple Creek Blvd #4, Markham, ON L3R 5Z1</div>
+        <div>Tel: (905) 307-8828</div>
+        <div>
+          <a href="https://www.facebook.com/pages/Master%20Q%20Billiards%20&%20Lounge/151066128275191/" target="_blank" rel="noopener">Facebook</a> • 
+          <a href="https://www.instagram.com/masterq.billiards/?hl=en" target="_blank" rel="noopener">Instagram</a>
+        </div>
+      </div>
+    ),
   },
+  primaryHue: 350, // Reddish tint
+  banner: {
+    key: 'grand-opening',
+    text: '🎉 Weekday Beer Special: $5 for all domestic beers and $6 for all imported beers!',
+  }
 }
 
 export default config
