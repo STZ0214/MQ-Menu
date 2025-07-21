c1a4ab2e73357597a07d924f623961cc6626583e
STZ0214 <steven.zhang.0214@gmail.com>
2025-07-21 16:29:01 -0400

    Reapply "V4.2 Testing"
    This reverts commit 6c5441483b0d9e962bf25088ac57a27a9370427c.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,13 @@
 import '../styles/globals.css';
+import { NextIntlClientProvider } from 'next-intl';
 
-export default function Nextra({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function Nextra({ Component, pageProps, router }) {
+  const locale = router.locale || 'en';
+  const messages = require(`../messages/${locale}.json`);
+
+  return (
+    <NextIntlClientProvider locale={locale} messages={messages}>
+      <Component {...pageProps} />
+    </NextIntlClientProvider>
+  );
 }
